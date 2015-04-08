8d346d3c9d930a67c58a28b87b1ab543c9367f6a
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-04-07 20:23:46 -0400

    Control Board Program to test components on the Control Board.

---
+++ src/org/usfirst/frc/team157/robot/commands/ChangeMotorControl.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc.team157.robot.commands;
+
+import org.usfirst.frc.team157.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Teju
+ */
+public class ChangeMotorControl extends Command
+{
+	
+	public ChangeMotorControl()
+	{
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
+	
+	// Called once after isFinished returns true
+	@Override
+	protected void end()
+	{
+		System.out.println("UseCAN changed to: " + RobotMap.useCAN);
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute()
+	{
+	}
+	
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize()
+	{
+		RobotMap.useCAN = !RobotMap.useCAN;
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted()
+	{
+	}
+	
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished()
+	{
+		return true;
+	}
+}
---
+++ src/org/usfirst/frc/team157/robot/commands/RetractPiston.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc.team157.robot.commands;
+
+import org.usfirst.frc.team157.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Teju
+ */
+public class RetractPiston extends Command
+{
+	
+	public RetractPiston()
+	{
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
+	
+	// Called once after isFinished returns true
+	@Override
+	protected void end()
+	{
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute()
+	{
+	}
+	
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize()
+	{
+		Robot.pneumaticSubsystem.retractPiston();
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted()
+	{
+	}
+	
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished()
+	{
+		return true;
+	}
+}
